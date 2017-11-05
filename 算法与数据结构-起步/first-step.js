ab6d662c7f0135023afbc8452e6b653c50d899de
Panda-HJN <376194715@qq.com>
2017-11-05 23:23:38 +0800

    今天开始学算法

---
+++ 算法与数据结构-起步/first-step.js
@@ -0,0 +1,18 @@
+function arrTest(num) {
+    console.time(`push出一个长度是${num}的数组所花时间`)
+    const arr1 = []
+    for (let i = 0; i < num; i++) {
+        arr1.push(i)
+    }
+    console.timeEnd(`push出一个长度是${num}的数组所花时间`)
+    console.time(`先new出一个长度是${num}的数组,再逐个修改值所花时间`)
+    const arr2 = new Array(num)
+    for (let i = 0; i < num; i++) {
+        arr2[i] = i
+    }
+    console.timeEnd(`先new出一个长度是${num}的数组,再逐个修改值所花时间`)
+    console.log('\n')
+}
+
+arrTest(22222222)
+arrTest(66666666)
